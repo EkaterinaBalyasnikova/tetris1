698682f5a880ff94d6546a1af7206cfed90b1bf0
EkaterinaBalyasnikova <esb17032003@gmail.com>
2022-06-04 10:19:54 +0300

    ok

--- main.cpp
+++ main.cpp
@@ -1,6 +1,192 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
+#include <time.h>
+
+using namespace sf;
+
+const int M = 30; //ширина
+const int N = 30; //длина
+int p = 18;
+
+int field[M][N] = {0}; //массив полей
+
+struct Point {
+    int x, y;
+} a[4], b[4];
+
+int figures[7][4] = { //массив с координатами закрашиваемых квадратов в фигуре
+        1, 3, 5, 7,
+        2, 4, 5, 7,
+        3, 5, 4, 6,
+        3, 5, 4, 7,
+        2, 3, 5, 7,
+        3, 5, 7, 6,
+        2, 3, 4, 5,
+};
+
+bool check() {
+    for (int i = 0; i < 4; i++)
+        if (a[i].x < 0 || a[i].x >= N || a[i].y >= M) return 0;
+        else if (field[a[i].y][a[i].x]) return 0;
+
+    return 1;
+};
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    srand(time(0));
+
+    RenderWindow window(VideoMode(M * p, N * p), "Tetris!");
+
+    Texture t;
+    t.loadFromFile("C:/Users/novik/CLionProjects/01 Tetris/images/tiles.png");
+
+    Sprite tiles(t);
+
+
+
+
+    int dx = 0, colorNum = 1;
+    bool rotate = false;
+    float timer = 0, delay = 0.3;
+    Clock clock;
+    bool ad = true;
+
+    int ga = 0; //Добавим коэффициент для конца игры
+
+    Texture go;
+    go.loadFromFile("C:/Users/novik/CLionProjects/01 Tetris/images/gameover.png");
+    Sprite gameover(go);
+    gameover.setPosition(15, 30);
+    gameover.setScale(1.7, 1.7);
+
+    while (window.isOpen()) {
+        float time = clock.getElapsedTime().asSeconds();
+        clock.restart();
+        timer += time;
+
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed)
+                window.close();
+
+            if (event.type == Event::KeyPressed)
+                if (event.key.code == Keyboard::Up)
+                    rotate = true;
+                else if (event.key.code == Keyboard::Right)
+                    dx = 1;
+                else if (event.key.code == Keyboard::Left)
+                    dx = -1;
+        }
+
+
+        if (Keyboard::isKeyPressed(Keyboard::Down))
+            delay = 0.05;
+
+        for (int i = 0; i < 4; i++) {
+            b[i] = a[i];
+            a[i].x += dx;
+        }
+
+        if (!check()) {
+            for (int i = 0; i < 4; i++)
+                a[i] = b[i];
+        }
+
+        if (rotate) {
+            Point p = a[1];
+            for (int i = 0; i < 4; i++) {
+                int x = a[i].y - p.y;
+                int y = a[i].x - p.x;
+
+                a[i].x = p.x - x;
+                a[i].y = p.y + y;
+            }
+
+            if (!check()) {
+                for (int i = 0; i < 4; i++)
+                    a[i] = b[i];
+            }
+        }
+
+        if (timer > delay) {
+            for (int i = 0; i < 4; i++) {
+                b[i] = a[i];
+                a[i].y += 1;
+            }
+
+            if (!check()) {
+                for (int i = 0; i < 4; i++)
+                    field[b[i].y][b[i].x] = colorNum;
+                colorNum = 1 + rand() % 7;
+                int n = rand() % 7;
+                for (int i = 0; i < 4; i++) {
+                    a[i].x = figures[n][i] % 2;
+                    a[i].y = figures[n][i] / 2;
+                }
+            }
+
+            timer = 0;
+        }
+
+
+        for (int i = 0; i < 4; i++) {
+            if (a[i].y > 0 && a[i].y < 4)
+                ga++;
+            else if (a[i].y >= 4)
+                ga = 0;
+        }
+
+        std::cout << ga << "\n";
+
+        if (ad) {
+            int n = rand() % 7;
+            if (a[0].x == 0)
+                for (int i = 0; i < 4; i++) {
+                    a[i].x = figures[n][i] % 2;
+                    a[i].y = figures[n][i] / 2;
+                }
+            ad = false;
+        }
+
+        int k = M - 1;
+        for (int i = M - 1; i > 0; i--) {
+            int count = 0;
+            for (int j = 0; j < N; j++) {
+                if (field[i][j])
+                    count++;
+                field[k][j] = field[i][j];
+            }
+            if (count < N)
+                k--;
+        }
+
+        dx = 0;
+        rotate = false;
+        delay = 0.3;
+
+        window.clear(Color::White);
+
+        for (int i = 0; i < M; i++)
+            for (int j = 0; j < N; j++) {
+                if (field[i][j] == 0)
+                    continue;
+                tiles.setTextureRect(IntRect(field[i][j] * p, 0, p, p));
+                tiles.setPosition(j * p, i * p);
+                window.draw(tiles);
+            }
+
+        for (int i = 0; i < 4; i++) {
+            tiles.setTextureRect(IntRect(colorNum * p, 0, p, p));
+            tiles.setPosition(a[i].x * p, a[i].y * p);
+            window.draw(tiles);
+        }
+
+        if (ga > 2000)
+            window.draw(gameover);
+
+        window.display();
+    }
+
     return 0;
 }
